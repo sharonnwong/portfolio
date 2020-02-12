c8ad7a74bd594e7eabf9a3c281682ca06026fbef
sharonnwong <shachi19@uw.edu>
2020-02-12 10:10:22 -0800

    first portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,94 @@
+let resumeData = {
+  name: "Sharon Wong",
+  role: "UX Designer and Inclusive Designer",
+  linkedinId:"sharon-wong19",
+  skypeid: "Your skypeid",
+  roleDescription: "I am a student in the Human Centered Design & Engineering Department at the University of Washington.",
+  socialLinks:[
+      {
+        name:"linkedin",
+        url:"https://www.linkedin.com/in/sharon-wong19",
+        className:"fa fa-linkedin"
+      },
+      {
+        name:"github",
+        url:"http://github.com/",
+        className:"fa fa-github"
+      },
+      {
+        name:"skype",
+        url:"http://skype.com",
+        className:"fa fa-skype"
+      }
+    ],
+  aboutme:"I am working toward my B.S. in Human Centered Design and Engineering, and am currently seeking a UX Design intern position or HCI research opportunity.",
+  address:"Seattle, Washington",
+  education:[
+    {
+      UniversityName:"University of Washington",
+      specialization:"Human Centered Design & Engineering (HCDE): Human Computer Interaction",
+      MonthOfPassing:"June",
+      YearOfPassing:"2019",
+      Achievements:"Interdisciplinary Honors Program"
+    }
+  ],
+  work:[
+    {
+      CompanyName:"UW HCDE",
+      specialization:"Course Assistant",
+      MonthOfLeaving:"Mar",
+      YearOfLeaving:"2020",
+      Achievements:"Some Achievements"
+    },
+    {
+      CompanyName:"First Code Academy",
+      specialization:"Coding Instructor",
+      MonthOfLeaving:"Sep",
+      YearOfLeaving:"2019",
+      Achievements:"Some Achievements"
+    }
+  ],
+  skillsDescription:"My skills:",
+  skills:[
+    {
+      skillname:"Reactjs",
+      level:90,
+    },
+    {
+      skillname:"HTML5",
+      level:70,
+    },
+    {
+      skillname:"CSS",
+      level: 80,
+    },
+  ],
+  portfolio:[
+    {
+      name:"project1",
+      description:"mobileapp",
+      imgurl:"images/portfolio/phone.jpg",
+      title:"Phone App",
+      githubLink:"https://github.com/Evanfeenstra/chatter",
+      fullDescription:"this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app "
+    },
+    {
+      name:"asdfasdfasf",
+      description:"mobileapp",
+      imgurl:"images/portfolio/weatherer.png",
+      title:"Something",
+      githubLink:"https://github.com",
+      fullDescription:"this is a longer description about this app"
+    },
+    {
+      name:"project3",
+      description:"mobileapp",
+      imgurl:"images/portfolio/chatter.jpg",
+      title:"Hello",
+      githubLink:"https://github.com",
+      fullDescription:"this is a longer description about this app"
+    },
+  ],
+}
+
+export default resumeData
